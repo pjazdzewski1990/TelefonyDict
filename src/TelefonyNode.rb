476d561fc9685d00d9e239d7838cedb2e827617a
jfk <jfk@jfk-VirtualBox.(none)>
2012-06-07 14:40:29 +0200

    search/index

--- src/TelefonyNode.rb
+++ src/TelefonyNode.rb
@@ -4,10 +4,10 @@
 # oraz do wyszukiwania zawartcyh w nim slow
 ##
 class TelefonyNode
-  value = ""  # wartosc tego wezla (litera)
-  children    # hash-mapa na dzieci tego noda
-              # klucz to litera, wartosc to node
-              # nil oznacza brak powiazania
+  @value = ""    # wartosc tego wezla (litera)
+  @children = {} # hash-mapa na dzieci tego noda
+                # klucz to litera, wartosc to node
+                # nil oznacza brak powiazania
   #  2 = A B C
   #  3 = D E F
   #  4 = G H I
@@ -16,7 +16,7 @@ class TelefonyNode
   #  7 = P Q R S
   #  8 = T U V
   #  9 = W X Y Z
-  mapping = {
+  @mapping = {
     "2" => ["A", "B", "C"],
     "3" => ["D", "E", "F"],
     "4" => ["G", "H", "I"],
@@ -32,6 +32,17 @@ class TelefonyNode
   # jedyny sposob na ich inicjalizacje to konstruktor 
   def initialize(word)
       @value = word
+      @children = {}
+      @mapping = {
+        "2" => ["A", "B", "C"],
+        "3" => ["D", "E", "F"],
+        "4" => ["G", "H", "I"],
+        "5" => ["J", "K", "L"],
+        "6" => ["M", "N", "O"],
+        "7" => ["P", "Q", "R", "S"],
+        "8" => ["T", "U", "V"],
+        "9" => ["W", "X", "Y", "Z"]
+      }
   end
   
   ##
@@ -44,12 +55,12 @@ class TelefonyNode
       return
     end 
     #dodaj dziecko
-    letter = word[0]
-    if children[letter]==nil
-      children[letter] = Telefony.new(letter)
+    letter = word[0].chr
+    if @children[letter]==nil
+      @children[letter] = TelefonyNode.new(letter)
     end
     #wywoluj rekursywnie
-    children[letter].addWord(word[1, word.length])
+    @children[letter].addWord(word[1, word.length])
   end
  
   ##
@@ -64,13 +75,28 @@ class TelefonyNode
       return
     end 
     #rozwazana litera
-    letter = word[0]
+    letter = word[0].chr
     #wywoluj rekursywnie, dla kazdego istniejacego dziecka
-    mapping[letter].each{|l|
-      if children[l]!=nil
-        children[l].search(word[1, word.length], acc+l)
+    @mapping[letter].each{ |l|
+      ##puts(l)
+      if @children[l]!=nil
+        @children[l].search(word[1, word.length], acc+l)
       end
     }
+  end
+  
+  ##
+  # Indeksuj plik o nazwie "fileName"
+  ##
+  def indexFile(fileName)
+    file = File.open(fileName)
+    line = file.readline
+    while line != nil do
+      ##line = capitalize(line)
+      addWord(line)
+      line = file.readline
+    end
+    file.close
   end
   
 end
--- src/main.rb
+++ src/main.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/Telefony.rb')
+require File.expand_path(File.dirname(__FILE__) + '/TelefonyNode.rb')
 #require '/Telefony.rb'
 
 if ARGV.size < 2
@@ -6,7 +6,16 @@ if ARGV.size < 2
   return
 end
 
-t = Telefony.new()
-found = t.search(ARGV[0], ARGV[1])
-puts("Znaleziono:")
-puts(found)
+t = TelefonyNode.new("")
+t.addWord("QWERTY")
+t.search("793789")
+t.addWord("QWE")
+t.search("793")
+t.addWord("ABC")
+t.search("222")
+t.addWord("AABB")
+t.search("2222")
+
+t.search(ARGV[0])
+#puts("Znaleziono:")
+#puts(found)
